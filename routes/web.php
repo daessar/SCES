63f9b4787301bebb2eee49a8c56e91dfa165e95c
GiselaCS <giselacriollo16@gmail.com>
2021-05-27 12:13:09 -0500

    cambios en el web

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', [InicioController::class, 'inicio']);
+//Route::get('/', [InicioController::class, 'inicio']);
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
+Route::get('/', function () {
+    return view('inicio');
+});
 
 Route::get('admin', function () {
     return  view('admin.dashboard');
