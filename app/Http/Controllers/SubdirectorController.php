435d4c4a91fcc59e7cc9923e60acdf5bfa325403
Vanesa Morales <vtmorales@misena.edu.co>
2021-05-27 12:10:59 -0500

    Modifique el SubdirectorController

--- app/Http/Controllers/SubdirectorController.php
+++ app/Http/Controllers/SubdirectorController.php
@@ -3,8 +3,95 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Subdirector;
+
+
 
 class SubdirectorController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+         return view('subdirector.index')
+            ->with('subdirectores', $this->subdirectores);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+         return view("subdirector.create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        /*$request->validate([
+            'nombre'=> 'required'
+        ]);
+        $posicion= new Posicion();
+        $posicion->nombre= $request->nombre;//request=corresponde al nombre del input del formulario.
+        $posicion->save();
+        return "Posición Guardada";*/
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+         return view("subdirector.show")
+        ->with('id',$id)
+        ->with('subdirector', $this->subdirectores[$id]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view ("subdirector.edit")
+        ->with ('id',$id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
